277dc69008f243f32a982d988300c75672eb3168
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-02-13 13:15:10 -0700

    Built the basic visuals for the board

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,12 +4,42 @@ import './Home.css';
 
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: [
+        ['R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R'],
+        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', ''],
+        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+        ['R', 'H', 'B', 'K', 'Q', 'B', 'H', 'R'],
+      ],
+    }
+  }
+
+  renderBoard() {
+    return this.state.board.map((row, i) => {
+      var squares = row.map( (item, j) => {
+        let styles = {
+          background: (j % 2 === i % 2) ? '#fff' : '#000',
+          color: (j % 2 === i % 2) ? '#000' : '#fff'
+        }
+        return <div className="square" key={j} style={styles} >{item}</div>;
+      })
+      return <div className='row' key={i} >{squares}</div>;
+    })
+  }
 
   render() {
     return (
       <div className="home">
 
-          Home Page
+        <div className='board'>
+          {this.renderBoard()}
+        </div>
 
       </div>
     );
