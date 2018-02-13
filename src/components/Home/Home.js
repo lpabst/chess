b718eabfac7d6129fad2bcd30fc677a63aec0909
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-02-13 15:54:32 -0700

    selected piece and squares contained in the available moves array highlight now for UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,41 +18,67 @@ class Home extends Component {
         ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
         ['br', 'bh', 'bb', 'bk', 'bq', 'bb', 'bh', 'br'],
       ],
+      whoseTurn: 'w',
       pieceSelected: false,
       pieceType: '',
-      selectedPieceLocation: []
+      selectedPieceLocation: [],
+      availableMoves: [[4,4], [6,3]],
     }
 
+    this.selectPiece = this.selectPiece.bind(this);
+    this.movePieceToEmptySquare = this.movePieceToEmptySquare.bind(this);
     this.clickSquare = this.clickSquare.bind(this);
     this.renderBoard = this.renderBoard.bind(this);
   }
 
+  selectPiece(i, j){
+    this.setState({
+      pieceSelected: true,
+      pieceType: this.state.board[i][j],
+      selectedPieceLocation: [i, j]
+    })
+  }
+
+  movePieceToEmptySquare(i, j){
+    let board = JSON.parse(JSON.stringify(this.state.board));
+    let oldLocation = this.state.selectedPieceLocation;
+    let newTurn = this.state.whoseTurn === 'w' ? 'b' : 'w';
+    board[i][j] = this.state.pieceType;
+    board[oldLocation[0]][oldLocation[1]] = '';
+
+    this.setState({
+      pieceSelected: false,
+      pieceType: '',
+      selectedPieceLocation: [],
+      board: board,
+      whoseTurn: newTurn
+    })
+  }
+
   clickSquare(i, j){
-    if (!this.state.pieceSelected && this.state.board[i][j]){
-      this.setState({
-        pieceSelected: true,
-        pieceType: this.state.board[i][j],
-        selectedPieceLocation: [i, j]
-      })
+    // if there's no piece selected and the user clicked on an a piece of their own color
+    if (!this.state.pieceSelected && this.state.board[i][j] && this.state.board[i][j].charAt(0) === this.state.whoseTurn){
+      this.selectPiece(i, j);
     }
+    // if there IS a piece selected, but the user clicks on a different piece of their own color, set the selected piece to the new selection
+    else if (this.state.pieceSelected && this.state.board[i][j].charAt(0) === this.state.pieceType.charAt(0)){
+      this.selectPiece(i, j);
+    }
+    // If there is a piece selected and the user clicks on an empty square
     else if (this.state.pieceSelected && !this.state.board[i][j]){
-      let board = JSON.parse(JSON.stringify(this.state.board));
-      let oldLocation = this.state.selectedPieceLocation;
-      board[i][j] = this.state.pieceType;
-      board[oldLocation[0]][oldLocation[1]] = ''
-      this.setState({
-        pieceSelected: false,
-        pieceType: '',
-        selectedPieceLocation: [],
-        board: board
-      })
+      this.movePieceToEmptySquare(i, j);
     }
   }
 
   renderBoard() {
     return this.state.board.map((row, i) => {
       var squares = row.map( (item, j) => {
-        return <Square key={j} piece={item} handleClick={this.clickSquare} location={[i, j]} />
+        return <Square key={j} 
+        piece={item} 
+        handleClick={this.clickSquare} 
+        location={[i, j]} 
+        selectedPieceLocation={this.state.selectedPieceLocation}
+        availableMoves={this.state.availableMoves} />
       })
       return <div className='row' key={i} >{squares}</div>;
     })
@@ -62,6 +88,7 @@ class Home extends Component {
     return (
       <div className="home">
 
+        <p className='turn'>{this.state.whoseTurn === 'w' ? 'White\'s turn' : "Black's turn"}</p>
         <div className='board'>
           { this.renderBoard() }
         </div>
--- src/components/Home/Square/Square.js
+++ src/components/Home/Square/Square.js
@@ -4,10 +4,22 @@ class Square extends Component {
 
     render() {
         let [i, j] = this.props.location;
-        let piece = this.props.piece;
+        let {piece, availableMoves} = this.props;
+        let squareBorder;
+
+        if (i === this.props.selectedPieceLocation[0] && j === this.props.selectedPieceLocation[1]){
+            squareBorder = '4px solid rgb(14, 255, 14)';
+        }else{
+            for (let z = 0; z < availableMoves.length; z++){
+                if (i === availableMoves[z][0] && j === availableMoves[z][1]){
+                    squareBorder = '4px solid rgb(89, 208, 255)';
+                }
+            }
+        }
 
         let squareStyle = {
-            background: (j % 2 === i % 2) ? '#bbb' : 'rgb(83, 50, 0)'
+            background: (j % 2 === i % 2) ? '#bbb' : 'rgb(83, 50, 0)',
+            border: squareBorder
         }
         let pieceStyle = piece ? {
             background: "url('/media/" + piece + ".jpg')center no-repeat",
