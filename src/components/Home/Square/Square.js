2ba9657fbe958bf32000c06d739c807d80ac8015
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-02-13 14:51:26 -0700

    added images for the pieces

--- src/components/Home/Square/Square.js
+++ src/components/Home/Square/Square.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 
-
 class Square extends Component {
-    
+
     render() {
         let [i, j] = this.props.location;
         let styles = {
-            background: (j % 2 === i % 2) ? '#fff' : '#000',
-            color: (j % 2 === i % 2) ? '#000' : '#fff'
-          }
+            background: (j % 2 === i % 2) ? '#fff' : 'rgb(83, 50, 0)'
+        }
+        let pieceStyle = {
+            background: "url('/media/"+this.props.piece+".jpg')center no-repeat"
+        }
 
         return (
-            <section className='square' style={styles} onClick={(e) => this.props.handleClick(i, j)} >
-                {this.props.piece}
-            </section>
+            <div className='square' style={styles} onClick={(e) => this.props.handleClick(i, j)} >
+                <div className='piece' style={pieceStyle} ></div>
+            </div>
         );
     }
 }
