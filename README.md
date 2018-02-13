681bf1dc8f3393adaad0c8e1dc242fc619329926
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-02-13 16:41:32 -0700

    built available moves for rook and pawns (except au passant and castling)

--- README.md
+++ README.md
@@ -1,29 +1,8 @@
-# React-Seed-Basic With Redux
+# Chess Game In React
 
-This is a fully functional basic React app seed. It comes with Redux already set up, and uses Node, Express, Massive, and SQL for the back end.
+This is a chess game built using the React Framework
 
-## To Use This Seed
+Current State: Able to move pieces in a turn based fashion. Partway through generating a list of available moves for selected piece
+Next Step: Figure out available moves for each piece 
+    - THEN limit the movement to those locaitons
 
-1. Clone this seed
-2. In the terminal, navigate to the project folder and run 'npm i'
-3. Create a 'config.js' file in the server folder (It is already ignored).
-
-#### Should look something like this
-```javascript
-module.exports = {
-  secret: "aetbmjljfcbn!%$*$nkrlhlkhnannalvlwher#$%66345nlk",
-  connection: {
-    host: //host (you'll find this info in your heroku db credentials),
-    port: //port,
-    database: //database,
-    user: //user,
-    password: //password
-    ssl: true
-  },
-  port: 8000
-};
-```
-Note - Having the backend run on port 8000 allows npm start to run on port 3000 at the same time. This allows you to build your front end using npm start, while the backend will still respond to local api calls.
-
-4. For development - Use two terminals. Navigate to the project folder on both, and run npm start on one, and nodemon on the other.
-5. For production - Navigate to the project folder and run npm run build. Once that is done, run nodemon.
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,65 +20,173 @@ class Home extends Component {
       ],
       whoseTurn: 'w',
       pieceSelected: false,
-      pieceType: '',
+      selectedPieceType: '',
       selectedPieceLocation: [],
-      availableMoves: [[4,4], [6,3]],
+      availableMoves: [],
     }
 
     this.selectPiece = this.selectPiece.bind(this);
+    this.getAvailableMoves = this.getAvailableMoves.bind(this);
     this.movePieceToEmptySquare = this.movePieceToEmptySquare.bind(this);
     this.clickSquare = this.clickSquare.bind(this);
     this.renderBoard = this.renderBoard.bind(this);
   }
 
-  selectPiece(i, j){
+  selectPiece(i, j) {
     this.setState({
       pieceSelected: true,
-      pieceType: this.state.board[i][j],
+      selectedPieceType: this.state.board[i][j],
       selectedPieceLocation: [i, j]
-    })
+    }, this.getAvailableMoves)
   }
 
-  movePieceToEmptySquare(i, j){
+  getAvailableMoves() {
+    let board = this.state.board;
+    let { selectedPieceType, selectedPieceLocation } = this.state;
+    let availableMoves = [];
+
+    let i = selectedPieceLocation[0];
+    let j = selectedPieceLocation[1];
+
+    // White Pawn (no need to watch for the end of the board, because on the last row it becomes something else and will have new movement rules)
+    if (selectedPieceType === 'wp') {
+      if (!board[i+1][j]){
+        availableMoves.push([i + 1, j]);
+      }
+      // First move allow 2 spaces in front
+      if (selectedPieceLocation[0] === 1 && !board[i+1][j] && !board[i+2][j]) {
+        availableMoves.push([i + 2, j]);
+      } 
+    } 
+    // Black Pawn
+    else if (selectedPieceType === 'bp') {
+      if (!board[i-1][j]){
+        availableMoves.push([i - 1, j]);
+      }
+      // First move allow 2 spaces in front
+      if (selectedPieceLocation[0] === 6 && !board[i-1][j] && !board[i-2][j]) {
+        availableMoves.push([i - 2, j]);
+      }
+    }
+    // Rook
+    else if (selectedPieceType.charAt(1) === 'r'){
+      j = selectedPieceLocation[1];
+
+      for (i = selectedPieceLocation[0] + 1; i <= 7 && i >= 0; i++){
+        if (!board[i][j]){
+          // if it's an empty space, add it as an available location
+          availableMoves.push([i, j]);
+        }else{
+          // otherwise, this is as far as we can go, so set i = board.length+1 and only add this 
+          // square as an available move it it's an opponent's piece
+          if (board[i][j].charAt(0) !== selectedPieceType.charAt(0)){
+            availableMoves.push([i, j]);
+          }
+          i = board.length + 1;
+        } 
+      }
+      for (i = selectedPieceLocation[0] - 1; i >= 0 && i <= 7; i--){
+        if (!board[i][j]){
+          // if it's an empty space, add it as an available location
+          availableMoves.push([i, j]);
+        }else{
+          // otherwise, this is as far as we can go, so set i = board.length+1 and only add this square as an available move it it's an opponent's piece
+          if (board[i][j].charAt(0) !== selectedPieceType.charAt(0)){
+            availableMoves.push([i, j]);
+          }
+          i = board.length + 1;
+        } 
+      }
+
+      i = selectedPieceLocation[0];
+
+      for (j = selectedPieceLocation[1] + 1; j <= 7 && j >= 0; j++){
+        if (!board[i][j]){
+          // if it's an empty space, add it as an available location
+          availableMoves.push([i, j]);
+        }else{
+          // otherwise, this is as far as we can go, so set i = board.length+1 and only add this 
+          // square as an available move it it's an opponent's piece
+          if (board[i][j].charAt(0) !== selectedPieceType.charAt(0)){
+            availableMoves.push([i, j]);
+          }
+          j = board.length + 1;
+        } 
+      }
+      for (j = selectedPieceLocation[1] - 1; j >= 0 && j <= 7; j--){
+        if (!board[i][j]){
+          // if it's an empty space, add it as an available location
+          availableMoves.push([i, j]);
+        }else{
+          // otherwise, this is as far as we can go, so set i = board.length+1 and only add this square as an available move it it's an opponent's piece
+          if (board[i][j].charAt(0) !== selectedPieceType.charAt(0)){
+            availableMoves.push([i, j]);
+          }
+          j = board.length + 1;
+        } 
+      }
+    }
+    // Horse
+    else if (selectedPieceType.charAt(1) === 'h'){
+      
+    }
+    // Bishop
+    else if (selectedPieceType.charAt(1) === 'b'){
+
+    }
+    // Queen
+    else if (selectedPieceType.charAt(1) === 'q'){
+
+    }
+    // King
+    else if (selectedPieceType.charAt(1) === 'k'){
+
+    }
+
+    this.setState({ availableMoves })
+  }
+
+  movePieceToEmptySquare(i, j) {
     let board = JSON.parse(JSON.stringify(this.state.board));
     let oldLocation = this.state.selectedPieceLocation;
     let newTurn = this.state.whoseTurn === 'w' ? 'b' : 'w';
-    board[i][j] = this.state.pieceType;
+    board[i][j] = this.state.selectedPieceType;
     board[oldLocation[0]][oldLocation[1]] = '';
 
     this.setState({
       pieceSelected: false,
-      pieceType: '',
+      selectedPieceType: '',
       selectedPieceLocation: [],
       board: board,
-      whoseTurn: newTurn
+      whoseTurn: newTurn,
+      availableMoves: [],
     })
   }
 
-  clickSquare(i, j){
+  clickSquare(i, j) {
     // if there's no piece selected and the user clicked on an a piece of their own color
-    if (!this.state.pieceSelected && this.state.board[i][j] && this.state.board[i][j].charAt(0) === this.state.whoseTurn){
+    if (!this.state.pieceSelected && this.state.board[i][j] && this.state.board[i][j].charAt(0) === this.state.whoseTurn) {
       this.selectPiece(i, j);
     }
     // if there IS a piece selected, but the user clicks on a different piece of their own color, set the selected piece to the new selection
-    else if (this.state.pieceSelected && this.state.board[i][j].charAt(0) === this.state.pieceType.charAt(0)){
+    else if (this.state.pieceSelected && this.state.board[i][j].charAt(0) === this.state.selectedPieceType.charAt(0)) {
       this.selectPiece(i, j);
     }
     // If there is a piece selected and the user clicks on an empty square
-    else if (this.state.pieceSelected && !this.state.board[i][j]){
+    else if (this.state.pieceSelected && !this.state.board[i][j]) {
       this.movePieceToEmptySquare(i, j);
     }
   }
 
   renderBoard() {
     return this.state.board.map((row, i) => {
-      var squares = row.map( (item, j) => {
-        return <Square key={j} 
-        piece={item} 
-        handleClick={this.clickSquare} 
-        location={[i, j]} 
-        selectedPieceLocation={this.state.selectedPieceLocation}
-        availableMoves={this.state.availableMoves} />
+      var squares = row.map((item, j) => {
+        return <Square key={j}
+          piece={item}
+          handleClick={this.clickSquare}
+          location={[i, j]}
+          selectedPieceLocation={this.state.selectedPieceLocation}
+          availableMoves={this.state.availableMoves} />
       })
       return <div className='row' key={i} >{squares}</div>;
     })
@@ -90,7 +198,7 @@ class Home extends Component {
 
         <p className='turn'>{this.state.whoseTurn === 'w' ? 'White\'s turn' : "Black's turn"}</p>
         <div className='board'>
-          { this.renderBoard() }
+          {this.renderBoard()}
         </div>
 
       </div>
