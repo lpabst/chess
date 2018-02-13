d5b204b9259a81a6fb2dc6c16e71f5e2242bd1d8
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-02-13 14:58:35 -0700

    got the images styled for the diff pieces

--- src/components/Home/Square/Square.js
+++ src/components/Home/Square/Square.js
@@ -4,15 +4,19 @@ class Square extends Component {
 
     render() {
         let [i, j] = this.props.location;
-        let styles = {
-            background: (j % 2 === i % 2) ? '#fff' : 'rgb(83, 50, 0)'
+        let piece = this.props.piece;
+
+        let squareStyle = {
+            background: (j % 2 === i % 2) ? '#bbb' : 'rgb(83, 50, 0)'
         }
-        let pieceStyle = {
-            background: "url('/media/"+this.props.piece+".jpg')center no-repeat"
+        let pieceStyle = piece ? {
+            background: "url('/media/" + piece + ".jpg')center no-repeat",
+            backgroundSize: 'cover'
         }
+        : {}
 
         return (
-            <div className='square' style={styles} onClick={(e) => this.props.handleClick(i, j)} >
+            <div className='square' style={squareStyle} onClick={(e) => this.props.handleClick(i, j)} >
                 <div className='piece' style={pieceStyle} ></div>
             </div>
         );
