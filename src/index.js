8fb3c7ff390281b3b8816764b05cd2b66dcb91a6
loren pabst <lorenpabst@gmail.com>
2018-02-17 09:18:53 -0700

    adjusted the styles to adjust for smaller screens

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import App from './App';
 import {unregister} from './registerServiceWorker';
 
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 
-import store from './store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
